5a9ac5cb7d79e99cb72608fafd8d9a0bfee774d9
kang-bjin <kangbj00@dongguk.edu>
2025-05-03 00:08:18 +0900

    init: Spring + JPA + Docker + MySQL 환경설정 및 연동 완료

---
+++ backend/src/main/java/com/ossproj/donjjul/backend/BackendApplication.java
@@ -0,0 +1,14 @@
+package com.ossproj.donjjul.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+		System.out.println("Hello world!");
+	}
+}
