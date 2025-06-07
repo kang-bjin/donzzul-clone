2a6b59b5bac768b58f8bfae0f9a7b45c2d470a93
sykim <syeone01@dgu.ac.kr>
2025-06-08 00:16:42 +0900

    EC2로 백엔드 배포 완료

--- src/backend/src/main/java/com/ossproj/donjjul/config/WebConfig.java
+++ src/backend/src/main/java/com/ossproj/donjjul/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 API 경로 허용
-                .allowedOrigins("http://localhost:3000") // 프론트 로컬 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("http://donzzul.netlify.app") // 프론트 주소
+                .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
