8a7514a1a2ed14891442d195b9800dc0afaf2734
sykim <syeone01@dgu.ac.kr>
2025-06-09 10:22:12 +0900

    되돌리기

--- src/backend/src/main/java/com/ossproj/donjjul/config/AppConfig.java
+++ src/backend/src/main/java/com/ossproj/donjjul/config/AppConfig.java
@@ -4,12 +4,11 @@ package com.ossproj.donjjul.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 
 @Configuration
 public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate(new SimpleClientHttpRequestFactory());
+        return new RestTemplate();
     }
 }
--- src/backend/src/main/java/com/ossproj/donjjul/dto/OcrResponseDto.java
+++ src/backend/src/main/java/com/ossproj/donjjul/dto/OcrResponseDto.java
@@ -1,8 +1,6 @@
 // src/backend/src/main/java/com/ossproj/donjjul/dto/OcrResponseDto.java
 package com.ossproj.donjjul.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDate;
@@ -11,11 +9,6 @@ import java.time.LocalDate;
 @Setter
 public class OcrResponseDto {
     private boolean success;
-
-    @JsonProperty("business_number")
     private String businessNumber;
-
-    @JsonProperty("pay_date")
-    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate payDate;
 }
